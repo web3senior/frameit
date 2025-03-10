bf860250991166721cd9b7633a0c9dc152199f2a
web3senior <atenyun@gmail.com>
2025-03-10 05:19:00 +0330

    Add lazy load

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,29 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, lazy } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { UpProvider } from './contexts/UpProvider.jsx'
 import './index.scss'
 import './styles/global.scss'
 
+// import Home from './routes/Home.jsx'
+// import Admin from './routes/Admin.jsx'
 
-import Home from './routes/Home.jsx'
-import Admin from './routes/Admin.jsx'
+const Home = React.lazy(() => import('./routes/Home.jsx'))
+const Admin = React.lazy(() => import('./routes/Admin.jsx'))
 
 const root = document.getElementById('root')
 
 createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route index element={<UpProvider><Home /></UpProvider>} />
+      <Route
+        index
+        element={
+          <UpProvider>
+            <Home />
+          </UpProvider>
+        }
+      />
       <Route path="admin" element={<Admin />} />
     </Routes>
   </BrowserRouter>
